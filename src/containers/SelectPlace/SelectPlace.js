f00dd059daac1a7e4ffd34f50f50713740cc9426
Rafał Kwiecień <kwiecienraf@gmail.com>
2018-05-08 22:29:28 +0200

    Added few places

--- src/containers/SelectPlace/SelectPlace.js
+++ src/containers/SelectPlace/SelectPlace.js
@@ -4,26 +4,50 @@ import SelectPlace from 'components/pages/SelectPlace';
 const SelectPlacePage = () => (
   <SelectPlace
     initialValues={{
-      place: 2,
+      place: 1,
     }}
     places={[
       {
         id: 1,
         city: 'Kraków',
-        street: 'Strzelców',
-        number: '3',
+        street: 'os. Strzelców',
+        number: '4',
       },
       {
         id: 2,
-        city: 'Kraków2',
-        street: 'Strzelców2',
+        city: 'Kraków',
+        street: 'os. Oświecenia',
         number: '32',
       },
       {
         id: 3,
-        city: 'Kraków3',
-        street: 'Strzelców3',
-        number: '33',
+        city: 'Kraków',
+        street: 'Akademiki PK',
+        number: '',
+      },
+      {
+        id: 4,
+        city: 'Kraków',
+        street: 'Park Tysiąclecia',
+        number: '',
+      },
+      {
+        id: 5,
+        city: 'Kraków',
+        street: 'AWF',
+        number: '',
+      },
+      {
+        id: 6,
+        city: 'Kraków',
+        street: 'os. Wysokie',
+        number: '6',
+      },
+      {
+        id: 7,
+        city: 'Kraków',
+        street: 'Centrum Sportowe Parkowa',
+        number: '',
       },
     ]}
     onSubmit={() => {}}
