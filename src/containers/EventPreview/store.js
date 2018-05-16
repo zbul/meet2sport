9ba1568cf593c11bc3caf8a612d278163ec9056c
Michał Ligęza <mligeziak@gmail.com>
2018-05-16 20:30:33 +0200

    get event data from api

--- src/containers/EventPreview/store.js
+++ src/containers/EventPreview/store.js
@@ -1,10 +1,16 @@
 import ApiManager from 'utils/ApiManager';
 import { Actions } from 'react-native-router-flux';
 
-const initialState = {};
+const initialState = {
+  date: '',
+  time: '',
+  discipline: '',
+  lookingFor: '',
+};
 
 const actions = {
   onJoin: 'EVENT_PREVIEW_ON_JOIN',
+  getEvent: 'EVENT_PREVIEW_GET_EVENT',
 };
 
 export const onJoin = (eventId) => {
@@ -22,11 +28,28 @@ export const onJoin = (eventId) => {
   });
 };
 
+export const getEvent = (eventId) => {
+  const promise = ApiManager.getOne('events', eventId);
+
+  return ({
+    type: actions.getEvent,
+    payload: promise,
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actions.onJoin:
       return {
         ...state,
+      };
+    case actions.getEvent:
+      return {
+        ...state,
+        date: action.payload.date,
+        time: action.payload.time,
+        discipline: action.payload.discipline,
+        lookingFor: action.payload.lookingFor,
       };
     default:
       return state;
